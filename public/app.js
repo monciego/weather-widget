660c09c6db339d1fd4c2e829c2460edea891e805
Jericho <jerichobantiquete@gmail.com>
2021-10-31 13:55:31 +0800

    created a time function

--- public/app.js
+++ public/app.js
@@ -1,1 +1,25 @@
-console.log("hello world");
+const time = document.getElementById("time");
+const greeting = document.getElementById("greeting");
+
+// show time
+const showTime = () => {
+  let today = new Date();
+
+  (hour = today.getHours()), (minute = today.getMinutes());
+
+  // set AM or PM
+  const amPm = hour >= 12 ? "PM" : "AM";
+
+  // 12 hour format
+  hour = hour % 12 || 12;
+
+  //   output time
+  time.innerHTML = `${hour}<span>:</span>${minute}`;
+
+  setTimeout(() => {
+    showTime();
+  }, 1000);
+};
+
+// run
+showTime();
